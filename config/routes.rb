e3a075dfa76af5562fcbe5a1e288db44f38938a0
unknown <mavericktesini@gmail.com>
2018-10-02 14:34:03 -0400

    add api/airplanes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # front end will serve new/edit pages
+  
+  namespace :api do 
+    resources :airplanes, except: [:new, :edit]
+  end
+
 end
